1ae2ac29f43bcec7cd751f643d6cbc0d52a91fd0
martin <kaweesimartinanderson@gmail.com>
2019-06-24 02:14:18 -0400

    learning the DOM

---
+++ scripts.js
@@ -0,0 +1,4 @@
+let button = document.getElementById('changeBackground');
+button.addEventLisdocument.body.style.backgroundColor = 'fuchsia';tener('click', () => {
+  document.body.style.backgroundColor = 'fuchsia';
+});
